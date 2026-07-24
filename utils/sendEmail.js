4e3feb8ddd2efdc559169a222f6461410a121be0
vaibhavi <vaibhavij2608@gmail.com>
2026-07-24 14:22:55 +0530

    fix email sending by forcing IPv4 connection

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,13 +1,16 @@
 import nodemailer from "nodemailer";
 
 const sendEmail = async ({ to, subject, html }) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-  });
+ const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  family: 4,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
 
   await transporter.sendMail({
     from: `"PrepGenius AI" <${process.env.EMAIL_USER}>`,
